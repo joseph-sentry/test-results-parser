084b62c6ab884f6fd58be84ca378b95e74fd819d
joseph-sentry <joseph.sawaya@sentry.io>
2023-11-15 10:59:41 -0500

    feat(reportlog): add parse_pytest_reportlog function
    Signed-off-by: joseph-sentry <joseph.sawaya@sentry.io>

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use pyo3::prelude::*;
 
 mod helpers;
 mod junit;
+mod pytest_reportlog;
 mod testrun;
 
 /// A Python module implemented in Rust.
@@ -9,5 +10,9 @@ mod testrun;
 fn testing_result_parsers(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<testrun::Testrun>()?;
     m.add_function(wrap_pyfunction!(junit::parse_junit_xml, m)?)?;
+    m.add_function(wrap_pyfunction!(
+        pytest_reportlog::parse_pytest_reportlog,
+        m
+    )?)?;
     Ok(())
 }
---
+++ src/pytest_reportlog.rs
@@ -0,0 +1,87 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+use pyo3::{exceptions::PyRuntimeError, prelude::*};
+
+use crate::testrun::Testrun;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct Location(String, i32, String);
+
+#[derive(Serialize, Deserialize, Default)]
+#[serde(default)]
+struct PytestLine {
+    #[serde(rename = "$report_type")]
+    report_type: String,
+    start: f64,
+    stop: f64,
+    location: Option<Location>,
+    when: String,
+    outcome: String,
+}
+
+#[pyfunction]
+pub fn parse_pytest_reportlog(filename: String) -> PyResult<Vec<Testrun>> {
+    let mut buf = String::new();
+    let f = File::open(&filename)?;
+
+    let mut testruns: Vec<Testrun> = Vec::new();
+
+    let mut reader = BufReader::new(f);
+
+    let mut val: PytestLine;
+    let mut saved_start_time: f64 = 0.0;
+
+    let mut lineno = 0;
+
+    loop {
+        match reader.read_line(&mut buf) {
+            Ok(0) => {
+                break;
+            }
+            Ok(_) => {
+                val = serde_json::from_str(buf.trim()).map_err(|err| {
+                    PyRuntimeError::new_err(format!("Error parsing json line  {}", err))
+                })?;
+                if val.report_type == "TestReport" {
+                    match val.when.as_str() {
+                        "setup" => {
+                            saved_start_time = val.start;
+                        }
+                        "teardown" => {
+                            let location = val.location.unwrap();
+                            let name = location.2;
+                            let testsuite = location.0;
+                            let outcome = val.outcome;
+                            let end_time = val.stop;
+
+                            let f_duration = end_time - saved_start_time;
+                            let duration = f_duration.to_string();
+
+                            testruns.push(Testrun {
+                                name,
+                                testsuite,
+                                duration,
+                                outcome,
+                            });
+                        }
+                        _ => (),
+                    }
+                }
+                buf.clear();
+                Ok(())
+            }
+            Err(err) => Err(PyRuntimeError::new_err(format!(
+                "Error reading line number {} from {}:  {}",
+                lineno, &filename, err
+            ))),
+        }?;
+        lineno += 1;
+    }
+
+    Ok(testruns)
+}
